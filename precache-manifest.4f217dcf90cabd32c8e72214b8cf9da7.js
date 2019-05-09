b08fbacfb219accd2fd5c8ffb56e5a091e32e4eb
githubprabin143 <prabinkarki643@gmail.com>
2019-05-09 18:02:06 +0530

    Updates

---
+++ precache-manifest.4f217dcf90cabd32c8e72214b8cf9da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6faaaa17c7d0283bbea4299d47851f3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "872c3f936b305092cd0e",
+    "url": "/static/css/2.a66ef25d.chunk.css"
+  },
+  {
+    "revision": "7179890c45a41a9b6f04",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "872c3f936b305092cd0e",
+    "url": "/static/js/2.c5b425f8.chunk.js"
+  },
+  {
+    "revision": "7179890c45a41a9b6f04",
+    "url": "/static/js/main.0666e13d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
